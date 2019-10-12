b8330a7b2dcf4f5aa58d182e00dd411820d0d6fa
Juhász <juhasz.barna12@gmail.com>
2019-10-12 23:49:29 +0200

    mátrix2

---
+++ Teszt1/src/main/java/teszt1.java
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+public class teszt1 {
+
+    public static void main(String args[])
+    {
+        
+        int[][] almák = new int[3][3];
+        
+        for (int i = 0; i < almák.length; i++)
+        {
+            for (int j = 0; j <almák.length; j++)
+            {
+                
+                almák[i][j] =  i + j;
+                System.out.println(almák[i][j]);
+                
+            }
+            
+        }
+        
+        int a = 2/3;
+        
+        
+    } 
+    
+}

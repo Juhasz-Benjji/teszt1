c7adb53ce777a2e7fb918b88fc9860520a902d3c
Benjji92 <bazsobeni@gmail.com>
2019-10-12 23:25:34 +0200

    mukodj

--- src/main/java/com/mycompany/teszt1/teszt1.java
+++ src/main/java/com/mycompany/teszt1/teszt1.java
@@ -10,13 +10,14 @@ package com.mycompany.teszt1;
  * @author Beni meg a faszom
  */
 public class teszt1 {
-
+//menj a kurva anyadba buzi netbeansdefwre4
     /**
      *
      * @param args
      */
     public static void main(String args[]){
         System.out.println("Faszom mukodjel mar te geci");
+        System.out.print("te geci");
     }
     
 }
